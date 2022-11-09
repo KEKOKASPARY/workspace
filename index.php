1f5b9df42834f72313d86392a574741d8ab30b0a
KEKOKASPARY <117411835+KEKOKASPARY@users.noreply.github.com>
2022-11-08 21:54:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require 'config.php';
+?>
+
+<<h1>Listagem de Usuário</h1>
+
+<<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <<th>Email</th>
+        <<th>Ações</th>
+    </tr>
+</table>
+
+<a href="">Cadastrar Usuário</a>
